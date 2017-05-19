6143fc5dd1b0f470425d4e337d0804acf372448f
Hieu Huynh <hieuhuynh@kinssolutions.net>
2017-05-19 23:33:43 +0700

    init root view controller

---
+++ MyProject/mvvm-structure/mvvm-structure/RootViewController.swift
@@ -0,0 +1,12 @@
+//
+//  RootViewController.swift
+//  mvvm-structure
+//
+//  Created by Hieu Huynh on 5/18/17.
+//  Copyright © 2017 Fraternal Group Pte. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+}
