067975c0b8de66c2ec5f2c372eb7930ecc16d29a
Hieu Huynh <hieuhuynh@kinssolutions.net>
2017-05-22 02:33:41 +0700

    apply mvvm for notes vc

--- MyProject/mvvm-structure/mvvm-structure/View/Controller/HHNotesViewController.swift
+++ MyProject/mvvm-structure/mvvm-structure/View/Controller/HHNotesViewController.swift
@@ -7,112 +7,54 @@
 //
 
 import UIKit
-import FirebaseDatabase
 
 class HHNotesViewController: UITableViewController {
-    // MARK: Constants
-    let cellToComposer = "CellToComposer"
-    let notesRef = FIRDatabase.database().reference(withPath: "notes")
     // MARK: Properties
-    var items: [HHNoteItem] = []
-    var user: User?
-    var authStateHandle: FIRAuthStateDidChangeListenerHandle?
-    var dataStateHandle: UInt = 0
-    var isUpToDate = false
-    var editingIndex = -1
+    var viewModel: HHNotesViewModel!
     // MARK: Overriden
     override func viewDidLoad() {
         super.viewDidLoad()
-        refreshControl = UIRefreshControl()
+        viewModel = HHNotesViewModel(viewController: self)
         tableView.allowsMultipleSelectionDuringEditing = false
+        refreshControl = UIRefreshControl()
         refreshControl?.addTarget(self, action: #selector(controlDidRefresh), for: .valueChanged)
         DispatchQueue.main.async {
             self.refreshControl?.beginRefreshing()
-            self.refreshData()
+            self.viewModel.refreshData()
         }
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        authStateHandle = FIRAuth.auth()!.addStateDidChangeListener { auth, user in
-            guard let user = user else { return }
-            self.user = User(authData: user)
-        }
+        viewModel.handleViewWillAppear()
     }
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        if let handle = authStateHandle {
-            FIRAuth.auth()?.removeStateDidChangeListener(handle)
-        }
+        viewModel.handleViewWillDisappear()
     }
     deinit {
-        notesRef.removeObserver(withHandle: dataStateHandle)
+        viewModel.handleDeinit()
     }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let noteComposerVC = segue.destination as? HHNoteComposerViewController {
-            noteComposerVC.delegate = self
-            // if it's edit mode, pass the existed data
-            if segue.identifier == cellToComposer, let cell = sender as? UITableViewCell {
-                if let indexPath = tableView.indexPath(for: cell) {
-                    let note = items[indexPath.row]
-                    noteComposerVC.note = note
-                    editingIndex = indexPath.row
-                }
-            }
-        }
+        viewModel.handlePrepare(for: segue, sender: sender)
     }
     // MARK: - Actions
     @IBAction func logoutPressed(_ sender: AnyObject) {
-        UIAlertController.show(in: self, withTitle: "Logout", message: "Are you sure you want to logout?", preferredStyle: .alert, cancelButtonTitle: "Cancel", destructiveButtonTitle: "Logout", otherButtonTitles: nil, popoverPresentationControllerBlock: nil) { [weak self](alertController, action, index) in
-            if index == UIAlertControllerBlocksDestructiveButtonIndex {
-                if let strongSelf = self {
-                    // perform logout
-                    Settings.email = nil
-                    do {
-                        try FIRAuth.auth()!.signOut()
-                        strongSelf.dismiss(animated: true, completion: nil)
-                    } catch {
-                        UIAlertController.showAlert(message: error.localizedDescription, from: strongSelf)
-                    }
-                }
-            }
-        }
+        viewModel.handleLogout()
     }
     // MARK: - fileprivate functions
     @objc fileprivate func controlDidRefresh() {
-        refreshData()
-    }
-    fileprivate func refreshData() {
-        // get data from FIR
-        // create a query that order data by last updated date
-        let notesQuery = notesRef.queryOrdered(byChild:"lastUpdated")
-        // observe query to get data
-        dataStateHandle =  notesQuery.observe(.value, with: { [weak self](snapshot) in
-            self?.items.removeAll()
-            // filter only notes from this user
-            let array = snapshot.children.filter{ item -> Bool in
-                let snpShot = item as! FIRDataSnapshot
-                let value = snpShot.value as! [String: AnyObject]
-                return (value["addedByUser"] as! String) == Settings.email
-            }
-            // iterate through items to add to current list
-            for item in array.reversed() { // reverse the list to make newest displayed first
-                let note = HHNoteItem(snapshot: item as! FIRDataSnapshot)
-                self?.items.append(note)
-            }
-            self?.tableView.reloadData()
-            self?.isUpToDate = true
-            self?.refreshControl?.endRefreshing()
-        })
+        viewModel.refreshData()
     }
 }
 // MARK: UITableView Delegate & Datasource methods
 extension HHNotesViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return viewModel.numberOfItems
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
-        let noteItem = items[indexPath.row]
+        let noteItem = viewModel.itemAt(index: indexPath.row)
         cell.textLabel?.text = noteItem.title == "" ? noteItem.content : noteItem.title
         var detailText = "\(noteItem.content ?? "")"
         if let lastUpdated = noteItem.lastUpdated {
@@ -126,51 +68,9 @@ extension HHNotesViewController {
     }
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            let noteObject = items[indexPath.row]
+            let noteObject = viewModel.itemAt(index: indexPath.row)
             noteObject.ref?.removeValue()
             tableView.reloadData()
         }
-    }
-}
-// MARK: HHNoteComposerVCDelegate
-extension HHNotesViewController: HHNoteComposerVCDelegate {
-    func noteComposerDidFinishComposing(note: HHNoteItem) {
-        isUpToDate = false
-        if note.ref == nil {
-            addNewNote(note)
-        } else {
-            updateNote(note)
-        }
-        self.tableView.reloadData()
-    }
-    func addNewNote(_ note: HHNoteItem) {
-        let titleCount = note.title?.characters.count ?? 0
-        let contentCount = note.content?.characters.count ?? 0
-        // ensure at least 1 characters was input for both title and content
-        guard (titleCount + contentCount) > 0 else {
-            return
-        }
-        var newNote = note
-        let noteRef = self.notesRef.childByAutoId()
-        newNote.ref = noteRef
-        noteRef.setValue(note.toAnyObject())
-        noteRef.setValue(note.toAnyObject()) {[weak self] (error, ref) in
-            guard self?.isUpToDate == false else {
-                return
-            }
-            self?.items.insert(newNote, at: 0)
-        }
-    }
-    func updateNote(_ note: HHNoteItem) {
-        var updateNote = note
-        updateNote.lastUpdated = Date().timeIntervalSince1970
-        updateNote.ref?.updateChildValues(updateNote.toAnyObject() as! [AnyHashable : Any], withCompletionBlock: { [weak self](error, ref) in
-            guard self?.isUpToDate == false else {
-                return
-            }
-            if let index = self?.editingIndex, index >= 0 {
-                self?.items[index] = note
-            }
-        })
     }
 }
---
+++ MyProject/mvvm-structure/mvvm-structure/ViewModel/HHNotesViewModel.swift
@@ -0,0 +1,143 @@
+//
+//  HHNotesViewModel.swift
+//  FireNotes
+//
+//  Created by Hieu Huynh on 5/22/17.
+//  Copyright © 2017 Fraternal Group Pte. Ltd. All rights reserved.
+//
+
+import Foundation
+
+import FirebaseDatabase
+class HHNotesViewModel {
+    // MARK: - Properties
+    fileprivate unowned let vc: HHNotesViewController
+    fileprivate let notesRef = FIRDatabase.database().reference(withPath: "notes")
+    fileprivate var authStateHandle: FIRAuthStateDidChangeListenerHandle?
+    fileprivate var dataStateHandle: UInt = 0
+    fileprivate var items: [HHNoteItem] = []
+    fileprivate var isUpToDate = false
+    fileprivate var user: User?
+    fileprivate var editingIndex = -1
+    fileprivate let cellToComposer = "CellToComposer"
+    // MARK: - Constructor
+    init(viewController: HHNotesViewController) {
+        vc = viewController
+    }
+    // MARK: - internal functions
+    var numberOfItems: Int {
+        return items.count
+    }
+    func itemAt(index: Int) -> HHNoteItem {
+        return items[index]
+    }
+    func handleLogout() {
+        UIAlertController.show(in: vc, withTitle: "Logout", message: "Are you sure you want to logout?", preferredStyle: .alert, cancelButtonTitle: "Cancel", destructiveButtonTitle: "Logout", otherButtonTitles: nil, popoverPresentationControllerBlock: nil) { [weak self](alertController, action, index) in
+            if index == UIAlertControllerBlocksDestructiveButtonIndex {
+                if let strongSelf = self {
+                    // perform logout
+                    Settings.email = nil
+                    do {
+                        try FIRAuth.auth()!.signOut()
+                        strongSelf.vc.dismiss(animated: true, completion: nil)
+                    } catch {
+                        UIAlertController.showAlert(message: error.localizedDescription, from: strongSelf.vc)
+                    }
+                }
+            }
+        }
+    }
+    func handlePrepare(`for` segue: UIStoryboardSegue, sender: Any?) {
+        if let noteComposerVC = segue.destination as? HHNoteComposerViewController {
+            noteComposerVC.delegate = self
+            // if it's edit mode, pass the existed data
+            if segue.identifier == cellToComposer, let cell = sender as? UITableViewCell {
+                if let indexPath = vc.tableView.indexPath(for: cell) {
+                    let note = items[indexPath.row]
+                    noteComposerVC.note = note
+                    editingIndex = indexPath.row
+                }
+            }
+        }
+    }
+    func handleDeinit() {
+        notesRef.removeObserver(withHandle: dataStateHandle)
+    }
+    func handleViewWillAppear() {
+        authStateHandle = FIRAuth.auth()!.addStateDidChangeListener { auth, user in
+            guard let user = user else { return }
+            self.user = User(authData: user)
+        }
+    }
+    func handleViewWillDisappear() {
+        if let handle = authStateHandle {
+            FIRAuth.auth()?.removeStateDidChangeListener(handle)
+        }
+    }
+    func refreshData() {
+        // get data from FIR
+        // create a query that order data by last updated date
+        let notesQuery = notesRef.queryOrdered(byChild:"lastUpdated")
+        // observe query to get data
+        dataStateHandle =  notesQuery.observe(.value, with: { [weak self](snapshot) in
+            self?.items.removeAll()
+            // filter only notes from this user
+            let array = snapshot.children.filter{ item -> Bool in
+                let snpShot = item as! FIRDataSnapshot
+                let value = snpShot.value as! [String: AnyObject]
+                return (value["addedByUser"] as! String) == Settings.email
+            }
+            // iterate through items to add to current list
+            for item in array.reversed() { // reverse the list to make newest displayed first
+                let note = HHNoteItem(snapshot: item as! FIRDataSnapshot)
+                self?.items.append(note)
+            }
+            self?.vc.tableView.reloadData()
+            self?.isUpToDate = true
+            self?.vc.refreshControl?.endRefreshing()
+        })
+    }
+    
+}
+// MARK: HHNoteComposerVCDelegate
+extension HHNotesViewModel: HHNoteComposerVCDelegate {
+    func noteComposerDidFinishComposing(note: HHNoteItem) {
+        isUpToDate = false
+        if note.ref == nil {
+            addNewNote(note)
+        } else {
+            updateNote(note)
+        }
+        vc.tableView.reloadData()
+    }
+    fileprivate func addNewNote(_ note: HHNoteItem) {
+        let titleCount = note.title?.characters.count ?? 0
+        let contentCount = note.content?.characters.count ?? 0
+        // ensure at least 1 characters was input for both title and content
+        guard (titleCount + contentCount) > 0 else {
+            return
+        }
+        var newNote = note
+        let noteRef = self.notesRef.childByAutoId()
+        newNote.ref = noteRef
+        noteRef.setValue(note.toAnyObject())
+        noteRef.setValue(note.toAnyObject()) {[weak self] (error, ref) in
+            guard self?.isUpToDate == false else {
+                return
+            }
+            self?.items.insert(newNote, at: 0)
+        }
+    }
+    func updateNote(_ note: HHNoteItem) {
+        var updateNote = note
+        updateNote.lastUpdated = Date().timeIntervalSince1970
+        updateNote.ref?.updateChildValues(updateNote.toAnyObject() as! [AnyHashable : Any], withCompletionBlock: { [weak self](error, ref) in
+            guard self?.isUpToDate == false else {
+                return
+            }
+            if let index = self?.editingIndex, index >= 0 {
+                self?.items[index] = note
+            }
+        })
+    }
+}
